11900d2433eb4b380b695977843d572bb15868f4
Joey Fondaco <Jfondacoo@gmail.com>
2018-12-22 12:46:28 -0500

    updating app.js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,17 +1,5 @@
-var spoonURL = "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/mealplans/generate?targetCalories=2000&timeFrame=day";
-
 hideMeals();
 hideExercise();
-
-$.ajax({
-    url: spoonURL,
-    method: "GET",
-    headers: {
-        'X-RapidAPI-Key': "zM5QZP2R1kmshOJ36ahyXh8O0o5zp1Pf94ojsnoBY9BXmViWZq"
-      }
-    }).then(function(response){
-        console.log(response);
-    });
 
     $("#meal-button").on("click", function(event){
         event.preventDefault();
@@ -20,7 +8,6 @@ $.ajax({
 
     $("#exercise-button").on("click", function(event){
         event.preventDefault();
-        console.log("hello");
         showExercise();
     });
 
@@ -43,3 +30,53 @@ $.ajax({
         $("#exercise-areas").show();
     }
     // hide/show functions end
+
+    //calorie calculator variables
+    var age = 0;
+    var weightPounds = 0;
+    var sex;
+    var heightInches = 0;
+    // activity factor will be static, 1.4 might change but assumes minimal activity?
+    var activityFactor = 1.4;
+
+    //on click for calorie result
+    $("#calorie-result").on("click", function(event){
+        event.preventDefault();
+        age = $("#calorie-age").val().trim();
+        weightPounds = $("#calorie-weight").val().trim();
+        heightInches = $("#calorie-height").val().trim();
+        sex = $("##########")
+
+        targetCalories = computeCalories();
+        console.log("Calories: "+targetCalories)
+
+        var spoonURL = "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/mealplans/generate?targetCalories="+targetCalories+"&timeFrame=day";
+
+        $.ajax({
+            url: spoonURL,
+            method: "GET",
+            headers: {
+                'X-RapidAPI-Key': "zM5QZP2R1kmshOJ36ahyXh8O0o5zp1Pf94ojsnoBY9BXmViWZq"
+              }
+            }).then(function(response){
+                console.log(response);
+            });
+
+
+    });
+    //end on click for calorie result
+
+    //formula function for daily calories
+    function computeCalories(weightPounds, heightInches, age, sex, activityFactor) {
+        var sexModifier;
+        if (sex === "male") {
+          sexModifier = 5;
+        } else {
+          sexModifier = -161;
+        }
+        var weightKilograms = weightPounds / 2.20462;
+        var heightCentimeters = 2.54 * heightInches;
+        var bmr = (10 * weightKilograms) + (6.25 * heightCentimeters) - (5 * age) + sexModifier;
+        return activityFactor * bmr;
+      }
+      
